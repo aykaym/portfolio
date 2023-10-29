f10cae00630f01c82bd11e86d5457e65f749bbd7
unknown <aykayem92@gmail.com>
2023-10-29 07:00:23 -0500

    more picture experiemntation

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -58,7 +58,7 @@ const AboutSection = () => {
         <section className="text-white">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image 
-                    src="/images/aboutme.jpg" 
+                    src="/static/images/aboutme.jpg" 
                     width={600} 
                     height={600}
                     alt="About Me"
